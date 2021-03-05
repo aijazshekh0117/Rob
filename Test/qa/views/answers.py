b20d69b0f8bd940b5ba8e57720c558a1a51f9110
Aijaz <aijaz@simplotel.com>
2021-03-05 20:25:06 +0530

    added logger

--- Test/qa/views/answers.py
+++ Test/qa/views/answers.py
@@ -8,6 +8,9 @@ from django.core.mail import send_mail
 from ..email.config import Config
 from rest_framework import status
 from rest_framework.response import Response
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class AnswerViewSet(ModelViewSet):
@@ -16,29 +19,42 @@ class AnswerViewSet(ModelViewSet):
     serializer_class = AnswerSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        admin_email = self.get_email(request)
-        subject = Config.ANSWER_SUBJECT + ': ' + request.data['Answer']
-        if admin_email and request.user.email:
-            send_mail(Config.ANSWER_SUBJECT, subject, admin_email, [request.user.email])
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        try:
+            serializer = self.get_serializer(data=request.data)
+            logger.info("Validating data.")
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            logger.info("Data saved Successfully.")
+            headers = self.get_success_headers(serializer.data)
+            admin_email = self.get_email(request)
+            subject = Config.ANSWER_SUBJECT + ': ' + request.data['Answer']
+            if admin_email and request.user.email:
+                logger.info("Sending Messeage..")
+                send_mail(Config.ANSWER_SUBJECT, subject, admin_email, [request.user.email])
+                logger.info("Sent Email.")
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except Exception as e:
+            logger.error(e)
 
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            instance._prefetched_objects_cache = {}
-        admin_email = self.get_email(request)
-        subject = Config.ANSWER_SUBJECT + ': ' + request.data['Answer']
-        if admin_email and request.user.email:
-            send_mail(Config.ANSWER_SUBJECT, subject, admin_email, [request.user.email])
+        try:
+            partial = kwargs.pop('partial', False)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            logger.info("Validating data.")
+            self.perform_update(serializer)
+            logger.info("Data saved Successfully.")
+            if getattr(instance, '_prefetched_objects_cache', None):
+                instance._prefetched_objects_cache = {}
+            admin_email = self.get_email(request)
+            subject = Config.ANSWER_SUBJECT + ': ' + request.data['Answer']
+            if admin_email and request.user.email:
+                logger.info("Sending Messeage..")
+                send_mail(Config.ANSWER_SUBJECT, subject, admin_email, [request.user.email])
+                logger.info("Sent Email.")
+        except Exception as e:
+            logger.error(e)
 
     def get_email(self, request):
         user_obj = User.objects.filter(username='admin').first()
--- Test/qa/views/questions.py
+++ Test/qa/views/questions.py
@@ -7,6 +7,9 @@ from django.core.mail import send_mail
 from ..email.config import Config
 from rest_framework import status
 from rest_framework.response import Response
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class QuestionViewSet(ModelViewSet):
@@ -15,29 +18,44 @@ class QuestionViewSet(ModelViewSet):
     serializer_class = QuestionSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        admin_email = self.get_email(request)
-        subject = Config.ANSWER_MESSAGE + ': ' + request.data['Question']
-        if request.user.email and admin_email:
-            send_mail(Config.QUESTION_MESSAGE, subject, request.user.email, [admin_email])
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        try:
+            serializer = self.get_serializer(data=request.data)
+            logger.info("Validating data.")
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            logger.info("Data swaved Successfully.")
+            headers = self.get_success_headers(serializer.data)
+            admin_email = self.get_email(request)
+            subject = Config.ANSWER_MESSAGE + ': ' + request.data['Question']
+            if request.user.email and admin_email:
+                logger.info("Sending Messeage..")
+                send_mail(Config.QUESTION_MESSAGE, subject, request.user.email, [admin_email])
+                logger.info("Sent Email.")
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except Exception as e:
+            logger.error("Found error while Saving Question data")
+            logger.error(e)
 
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            instance._prefetched_objects_cache = {}
-        admin_email = self.get_email(request)
-        subject = Config.ANSWER_MESSAGE + ': ' + request.data['Question']
-        if request.user.email and admin_email:
-            send_mail(Config.QUESTION_MESSAGE, subject, request.user.email, [admin_email])
+        try:
+            partial = kwargs.pop('partial', False)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            logger.info("Validating data.")
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            logger.info("Data saved Successfully.")
+            if getattr(instance, '_prefetched_objects_cache', None):
+                instance._prefetched_objects_cache = {}
+            admin_email = self.get_email(request)
+            subject = Config.ANSWER_MESSAGE + ': ' + request.data['Question']
+            if request.user.email and admin_email:
+                logger.info("Sending Messeage..")
+                send_mail(Config.QUESTION_MESSAGE, subject, request.user.email, [admin_email])
+                logger.info("Sent Email.")
+        except Exception as e:
+            logger.error("found error While updating the Question data")
+            logger.error(e)
 
     def get_email(self, request):
         user_obj = User.objects.filter(username='admin').first()
